8d03f57f994627516256b3af0261857be97c9c55
emialyx <15770877537@163.com>
2025-02-19 17:44:37 +0800

    txtsource update

--- globalfile.h
+++ globalfile.h
@@ -1,6 +1,6 @@
 #pragma once
-#define ADMIN_FILE "admin.txt"
-#define STUDENT_FILE "student.txt"
-#define TEACHER_FILE "teacher.txt"
-#define COMPUTER_FILE "computer.txt"//机房信息文件
-#define APPLY_FILE "apply.txt"//申请信息
+#define ADMIN_FILE "./txt source/admin.txt"
+#define STUDENT_FILE "./txt source/student.txt"
+#define TEACHER_FILE "./txt source/teacher.txt"
+#define COMPUTER_FILE "./txt source/computer.txt"//机房信息文件
+#define APPLY_FILE "./txt source/apply.txt"//申请信息
